9ce2a1008e10807a0bfead568e9b5307934e1195
Gaoyang Li <gaoyang.li-2@student.manchester.ac.uk>
2022-05-09 20:52:20 +0100

    .

---
+++ src/driver/board/CardList.java
@@ -0,0 +1,31 @@
+package board;
+
+import cards.Card;
+
+import java.util.ArrayList;
+
+public class CardList {
+
+    private ArrayList<Card> cList;
+
+    public CardList() {
+        cList = new ArrayList<>();
+    }
+
+    public void add(Card card) {
+        cList.add(card);
+    }
+
+    public int size() {
+        return cList.size();
+    }
+
+    public Card getElementAt(int i) {
+        return cList.get(i);
+    }
+
+    public Card removeCardAt(int i) {
+        return cList.remove(i);
+    }
+
+}
