5d9f0e4e7b3b459236efb471d63fa2fc7d32fc4f
Gaoyang Li <gaoyang.li-2@student.manchester.ac.uk>
2022-05-09 21:13:27 +0100

    .

---
+++ src/driver/cards/HenOfWoods.java
@@ -0,0 +1,11 @@
+package cards;
+
+public class HenOfWoods extends Mushroom{
+
+    public HenOfWoods(CardType type) {
+        super(type, "henofwoods");
+        flavourPoints = 3;
+        sticksPerMushroom = 1;
+    }
+
+}
